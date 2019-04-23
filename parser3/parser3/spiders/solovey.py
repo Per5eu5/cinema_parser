4a41557345e36b4be9419aded3ac37cf08e799ca
Per5eu5 <pavel7557@hotmail.com>
2019-04-23 08:04:18 +0300

    add pagination between main pages

--- parser3/parser3/spiders/solovey.py
+++ parser3/parser3/spiders/solovey.py
@@ -9,31 +9,47 @@ import re
 import datetime
 
 
-class SoloveySpider(CrawlSpider):
+class SoloveySpider(scrapy.Spider):
     name = 'solovey'
 
+    # def start_requests(self):
+    #     yield scrapy.Request('https://kinocenter.ru/repertoire/', self.parse_main_page)
+    #     yield scrapy.Request('https://kinocenter.ru/repertoire/tomorrow.php', self.parse_main_page)
 
-    start_urls = [#'https://kinocenter.ru/repertoire/',
-                  'https://kinocenter.ru/repertoire/tomorrow.php']
+    start_urls = ['https://kinocenter.ru/repertoire/']
+                  #'https://kinocenter.ru/repertoire/tomorrow.php']
 
-    rules = (
-        #Rule(LinkExtractor(allow=('https://kinocenter.ru/repertoire/',), deny=('movie', 'tomorrow', 'time', 'show', 'halls', 'genre', 'novelties',)), callback='parse_main_pages'),
-        Rule(LinkExtractor(allow=('kinocenter.ru/repertoire/tomorrow.php',), deny=('movie', 'genre', 'show')), callback='parse_main_page'),
+    # rules = (
+    #     Rule(LinkExtractor(allow=('kinocenter.ru/repertoire/',), deny=('movie', 'tomorrow', 'time', 'show', 'halls', 'genre', 'novelties',)), callback='parse_main_pages'),
+    #     Rule(LinkExtractor(allow=('kinocenter.ru/repertoire/tomorrow.php',), deny=('movie', 'genre', 'show')), callback='parse_main_page'),
+    #     Rule(LinkExtractor(allow=('movie')), callback='parse_detail_page'),
+    # )
 
-        # Rule(LinkExtractor(allow=('427889')), callback='parse_detail_page'),
-        # Rule(LinkExtractor(restrict_xpaths='//div[@class="film_blocks active"]'), callback='parse_main_page'),
-        Rule(LinkExtractor(allow=('movie')), callback='parse_detail_page'),
-        #Rule(LinkExtractor(restrict_xpaths='//div[@class="film_blocks active"]'), callback='parse_detail_page'),
+    def parse(self, response):
+        dates_in_schedule = response.xpath('//select[@id="date"]')
+        date_in_schedule = dates_in_schedule.xpath('option[@value]/text()').extract()
+       # print(len(date_in_schedule))
 
-    )
+        for i, date in enumerate(date_in_schedule):
+            #print(date)
+
+            if i == 0:
+                yield scrapy.Request('https://kinocenter.ru/repertoire/', callback=self.parse_main_page)
+            elif i == 1:
+                yield scrapy.Request('https://kinocenter.ru/repertoire/tomorrow.php', callback=self.parse_main_page)
+            elif i > 1:
+                yield scrapy.Request('https://kinocenter.ru/repertoire/date.php?date=' + date,
+                                    callback=self.parse_main_page)
+
+
+
 
     def parse_main_page(self, response):
-        print('parse')
         items = Parser3Item()
+        current_date = response.xpath('//select[@id="date"]/option[@selected]/text()').extract()
+        #print(current_date)
 
         all_films = response.xpath('//div[@class="item"][@style]')
-        dates = response.xpath('//select[@id="date"]')
-        print(dates)
 
         for film in all_films:
             title = film.xpath('div[@class="description"]/a/text()').extract()
@@ -43,7 +59,7 @@ class SoloveySpider(CrawlSpider):
             halls = film.xpath('div[@class="description"]/div[@class="links"]/div[@class="hall"]/text()').extract()
             poster = film.xpath('@style').extract()
             link = film.xpath('div[@class="description"]/a[@href]').extract()
-            date = dates.xpath('option[@selected]/text()').extract()
+            date = current_date
 
             items['title'] = title[0].strip()  # нужно значение элемента, чтобы применить strip()
             items['age'] = age
@@ -54,22 +70,24 @@ class SoloveySpider(CrawlSpider):
                                                                           "url('", "").replace("');", "")
             items['link'] = 'https://kinocenter.ru/repertoire/movie/?movie_id=' + re.findall('[0-9]+', link[0])[0]
             # нули потому что возвращаются списки
+            #yield scrapy.Request(items['link'], self.parse_detail_page)
+
             items['date'] = date
 
             yield items
 
     def parse_detail_page(self, response):
-
-        items = Detail()
-
-        #description = response.xpath('//div [@class="movie_description"]/div[@class="description"]/text()').extract()
-        cost = response.xpath('//div[@class="cost"]/text()').extract()
-        start_times_detail = response.xpath('//div[@class="time"]/text()').extract()
-        #items["description"] = description
-        items['cost'] = cost
-        items['start_times_detail'] = start_times_detail
-
-        yield items
+        print('jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj')
+        # items = Detail()
+        #
+        # #description = response.xpath('//div [@class="movie_description"]/div[@class="description"]/text()').extract()
+        # cost = response.xpath('//div[@class="cost"]/text()').extract()
+        # start_times_detail = response.xpath('//div[@class="time"]/text()').extract()
+        # #items["description"] = description
+        # items['cost'] = cost
+        # items['start_times_detail'] = start_times_detail
+        #
+        # yield items
 
 
 
